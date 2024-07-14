955ec5d126f8008d18a4f4a18b01e37f0a0ca187
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2024-07-14 22:13:29 +0530

    CHG | Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Header from './layouts/Header';
+import CategoryList from './pages/CategoryList';
+import BlogPostList from './pages/BlogPostList';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/category-list" element={<CategoryList />} />
+          <Route path="/blogPost-list" element={<BlogPostList />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
