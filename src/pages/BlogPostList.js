32e132eadac7867e351e16e36bb2aae4cb79ed3b
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2024-07-19 22:53:11 +0530

    CHG | Get All Blogposts

--- src/pages/BlogPostList.js
+++ src/pages/BlogPostList.js
@@ -1,20 +1,63 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import { toast } from "react-toastify";
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; 
 
 const BlogPostList = () => {
 
   const [blogPosts, setBlogPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     getAllBlogPosts();
   }, []);
 
-  const getAllBlogPosts = () => {
-    console.log("All Blog Posts"); 
+  const getAllBlogPosts = async () => {
+    try {
+      const { data } = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/Blogs`);
+      console.log(data);
+      setBlogPosts(data);
+      setIsLoading(false);
+    } catch (error) {
+      toast.error(error.message);
+      console.log(error);
+      setIsLoading(false);
+    } 
   }
   return (
-    <div>
-      Blog Post List
+    <div className="container">
+      <h1 className="mt-3">BlogPost List</h1>
+
+      <div className="d-flex justify-content-end mt-3">
+          <a  className="btn btn-primary">Add BlogPost</a>
+      </div>
+      <table className="table table-bordered mt-3">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Short Description</th>
+            <th>Is Visible?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {blogPosts.map((blogPost) => (
+            <tr key={blogPost.id}>
+              <td>{blogPost.title}</td>
+              <td>{blogPost.shortDescription}</td>
+              <td>{blogPost.isVisible ? 'True' : 'False'}</td>
+              {/* <td>
+                <button className="btn btn-secondary" onClick={() => editCategory(category.id)}>
+                  Edit
+                </button>
+                <button className="btn btn-danger ms-3" onClick={() => deleteCategory(category.id)}>
+                  Delete
+                </button>
+              </td> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
