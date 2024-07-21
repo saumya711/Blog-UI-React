1441cc961e31cab0630e0181eef2d9f3e12f3ca9
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2024-07-21 23:11:41 +0530

    CHG | Update Add Blogpost UI

--- src/components/blogposts/AddBlogPost.js
+++ src/components/blogposts/AddBlogPost.js
@@ -3,7 +3,6 @@ import { toast } from "react-toastify";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-
 const initialState = {
     title: "",
     urlHandle: "",
@@ -13,139 +12,136 @@ const initialState = {
     publishedDate: new Date(),
     author: "",
     isVisible: false,
-}
+};
 
 const AddBlogPost = () => {
-  const [values, setValues] = useState(initialState);
-  const navigate = useNavigate();
+    const [values, setValues] = useState(initialState);
+    const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setValues({ 
-      ...values, 
-      [name]: type === 'checkbox' ? checked : value 
-    });
-  };
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setValues({
+            ...values,
+            [name]: type === 'checkbox' ? checked : value
+        });
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(values);
-    try {
-      await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/Blogs`, values);
-      toast.success("Blog post created successfully");
-      navigate('/admin/blogPost-list');
-    } catch (error) {
-      toast.error(error.message);
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(values);
+        try {
+            await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/Blogs`, values);
+            toast.success("Blog post created successfully");
+            navigate('/admin/blogPost-list');
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
 
-  return (
-    <div className='container'>
-      <h1 className="mt-3">Add Blog Post</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mt-2 mb-5">
-            <div className="mt-3">
-                <label for="title" className="form-label">Title</label>
-                <input 
-                    type="text" 
-                    value={values.title} 
-                    className="form-control" 
-                    name="title"
-                    onChange={handleChange}
-                />
-            </div>
+    return (
+        <div className='container'>
+            <h1 className="mt-3 text-start">Add Blog Post</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="mt-2 mb-5">
+                    <div className="mt-3 text-start">
+                        <label htmlFor="title" className="form-label">Title</label>
+                        <input
+                            type="text"
+                            value={values.title}
+                            className="form-control"
+                            name="title"
+                            onChange={handleChange}
+                        />
+                    </div>
 
-            <div className="mt-3">
-                <label for="urlHandle" className="form-label">Url Handle</label>
-                <input 
-                    type="text" 
-                    value={values.urlHandle} 
-                    className="form-control" 
-                    name="urlHandle"
-                   onChange={handleChange}
-                />
-            </div>
+                    <div className="mt-3 text-start">
+                        <label htmlFor="urlHandle" className="form-label">Url Handle</label>
+                        <input
+                            type="text"
+                            value={values.urlHandle}
+                            className="form-control"
+                            name="urlHandle"
+                            onChange={handleChange}
+                        />
+                    </div>
 
-            <div className="mt-3">
-                <label for="shortDescription" className="form-label">Short Description</label>
-                <input 
-                    type="text" 
-                    value={values.shortDescription} 
-                    className="form-control" 
-                    name="shortDescription"
-                    onChange={handleChange}
-                />
-            </div>
+                    <div className="mt-3 text-start">
+                        <label htmlFor="shortDescription" className="form-label">Short Description</label>
+                        <input
+                            type="text"
+                            value={values.shortDescription}
+                            className="form-control"
+                            name="shortDescription"
+                            onChange={handleChange}
+                        />
+                    </div>
 
-            <div className="mt-3">
-                <label for="content" className="form-label">Content</label>
-                <textarea 
-                    name="content" 
-                    id="content" 
-                    className="form-control"
-                    value={values.content}
-                    onChange={handleChange}
-                >
-                </textarea>
-            </div>
+                    <div className="mt-3 text-start">
+                        <label htmlFor="content" className="form-label">Content</label>
+                        <textarea
+                            name="content"
+                            id="content"
+                            className="form-control"
+                            value={values.content}
+                            onChange={handleChange}
+                        >
+                        </textarea>
+                    </div>
 
-            <div className="mt-3">
-                <label for="featuredImageUrl" className="form-label">Featured Image Url</label>
-                <input 
-                    type="text" 
-                    value={values.featuredImageUrl} 
-                    className="form-control" 
-                    name="featuredImageUrl"
-                    onChange={handleChange}
-                />
-            </div>
+                    <div className="mt-3 text-start">
+                        <label htmlFor="featuredImageUrl" className="form-label">Featured Image Url</label>
+                        <input
+                            type="text"
+                            value={values.featuredImageUrl}
+                            className="form-control"
+                            name="featuredImageUrl"
+                            onChange={handleChange}
+                        />
+                    </div>
 
-            <div className="mt-3">
-                <label for="publishedDate" className="form-label">Published Date</label>
-                <input 
-                    type="date" 
-                    value={values.publishedDate} 
-                    className="form-control" 
-                    name="publishedDate"
-                    onChange={handleChange}
-                    // [ngModel]="model.publishedDate | date: 'yyy-MM-dd'"
-                    // (ngModelChange)="model.publishedDate = $event"
-                />
-            </div>
+                    <div className="mt-3 text-start">
+                        <label htmlFor="publishedDate" className="form-label">Published Date</label>
+                        <input
+                            type="date"
+                            value={values.publishedDate}
+                            className="form-control"
+                            name="publishedDate"
+                            onChange={handleChange}
+                        />
+                    </div>
 
-            <div className="mt-3">
-                <label for="author" className="form-label">Author</label>
-                <input 
-                    type="text" 
-                    value={values.author} 
-                    className="form-control" 
-                    name="author"
-                    onChange={handleChange}
-                />
-            </div>
+                    <div className="mt-3 text-start">
+                        <label htmlFor="author" className="form-label">Author</label>
+                        <input
+                            type="text"
+                            value={values.author}
+                            className="form-control"
+                            name="author"
+                            onChange={handleChange}
+                        />
+                    </div>
 
-            <div className="form-check mt-3">
-                <input 
-                    className="form-check-input" 
-                    type="checkbox" 
-                    id="isVisible" 
-                    name="isVisible"
-                    checked={values.isVisible}
-                    onChange={handleChange}
-                />
-                <label className="form-check-label  ms-2" for="isVisible">
-                 Is Visible?
-                </label>
-            </div>
+                    <div className="form-check mt-3 text-start">
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            id="isVisible"
+                            name="isVisible"
+                            checked={values.isVisible}
+                            onChange={handleChange}
+                        />
+                        <label className="form-check-label ms-2" htmlFor="isVisible">
+                            Is Visible?
+                        </label>
+                    </div>
 
-            <div className="mt-3">
-                <button type="submit" className="btn btn-primary">Save</button>
-            </div>
-
+                    <div className="mt-3 text-start">
+                        <button type="submit" className="btn btn-primary">Save</button>
+                    </div>
+                </div>
+            </form>
         </div>
-    </form>
-    </div>
-  )
-}
+    );
+};
 
-export default AddBlogPost
+export default AddBlogPost;
